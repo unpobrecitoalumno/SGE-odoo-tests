10d02a10a7821a1740502059f6629184e8e7892d
unpobrecitoalumno <unpobrecitoalumno@canaveral.sge>
2025-02-21 15:02:02 +0100

    Tests from 192.168.146.250 on 20250221_141944

--- test_verbose_report.sh
+++ test_verbose_report.sh
@@ -8,7 +8,7 @@ PRIVATE_IP=$(ifconfig $(route -n |awk '/0[.]0[.]0[.]0/{print $NF;exit}') | awk '
 cd "${TESTER_PATH}"
 source run.sh
 source .venv/bin/activate
-main "$FUNCION" "updated"
+main "$FUNCION" "all" # "updated"
 
 cp ${TESTER_PATH}/projects/${RESULTS_DIR}/${REPORTS_DIR}/* "${PUBLIC_PATH}/docs/" 
 cp ${TESTER_PATH}/projects/${RESULTS_DIR}/*-git.log "${PUBLIC_PATH}/docs/"

9dd6bd560b49b8ef0b8de60ae981dfce43100dfd
Ubuntu <ubuntu@ip-172-31-24-143.ec2.internal>
2026-01-11 14:08:13 +0000

    Tests from 172.31.24.143 on 20260111_135849

---
+++ test_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/ubuntu/SGE-odoo-tests/test_verbose_report.sh A all
+/home/ubuntu/SGE-odoo-tests/test_verbose_report.sh B all
+/home/ubuntu/SGE-odoo-tests/test_verbose_report.sh V all
